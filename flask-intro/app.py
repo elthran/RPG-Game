51ab6ff639b6a3ff1779df70e0c36659c665c197
elthran <jacobbrunner@gmail.com>
2016-08-03 00:08:27 -0700

    added class select

--- flask-intro/app.py
+++ flask-intro/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, url_for, request, session, f
 from functools import wraps
 
 class Hero(object):
-    def __init__(self, name, level, strength, agility, wisdom, vitality, hp, maxhp, wins):
+    def __init__(self, name, level, strength, agility, wisdom, vitality, hp, maxhp, wins, spec):
         self.name = name
         self.level = level
         self.strength = strength
@@ -13,11 +13,12 @@ class Hero(object):
         self.hp = hp
         self.maxhp = maxhp
         self.wins = wins
+        self.spec = spec
 
     def __repr__(self):
         return "Name: %s \n Class: Brute \n Level: %s \n Strength: %s \n Agility: %s \n Wisdom: %s \n Vitality: %s \n HP: %s/%s \n" % (self.name, self.level, self.strength, self.agility, self.wisdom, self.vitality, self.hp, self.maxhp) 
-myHero = Hero("Unknown", 1, 3, 4, 2, 3, 50, 50, 1)
-enemy = Hero("Goblin", 1, 2, 2, 2, 2, 7, 7, 0)
+myHero = Hero("Unknown", 1, 3, 4, 2, 3, 50, 50, 1, "")
+enemy = Hero("Goblin", 1, 2, 2, 2, 2, 7, 7, 0, "")
 
 # create the application object
 app = Flask(__name__)
@@ -99,7 +100,8 @@ def battle_results():
 @login_required
 def createcharacter():
     if request.method == 'POST':
-        myHero.name = request.form['name']
+        myHero.name = request.form['char_name']
+        myHero.spec = request.form['spec']
         return redirect(url_for('profile'))
     return render_template('createcharacter.html')  # return a string
 
