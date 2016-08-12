1303c5f5050e8ed8963fe7dde39e1cb188f5e5d0
elthran <jacobbrunner@gmail.com>
2016-08-12 15:38:09 -0700

    deleting html

--- flask-intro/app.py
+++ flask-intro/app.py
@@ -22,19 +22,6 @@ def login_required(f):
     return wrap
 
 # use decorators to link the function to a url
-@app.route('/')
-@login_required
-def home():
-    if myHero.name != "Unknown":
-        created = "OK"
-    else:
-        created = None
-    return render_template('home.html', created=created, myHero=myHero)  # return a string
-
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')  # render a template
-
 @app.route('/level_up')
 def level_up():
     return render_template('level_up.html')  # render a template
@@ -76,19 +63,19 @@ def profile():
         myHero.attribute_points -= int(request.form['vitality_upgrade'])
         myHero.set_health(myHero.endurance, myHero.vitality, myHero.max_hp)
         myHero.set_damage(myHero.strength)
-    return render_template('profile.html', page_title="Profile", myHero=myHero, profile=profile, sidebar=sidebar)  # return a string'
+    return render_template('home.html', profile=profile, page_title="Profile", myHero=myHero)  # return a string'
 
 @app.route('/arena')
 @login_required
 def arena():
     page_greeting= "Welcome to the arena " + myHero.name +"!"
-    return render_template('profile.html', page_title="Arena Results", page_greeting=page_greeting, myHero=myHero, arena=arena, sidebar=sidebar)  # return a string
+    return render_template('home.html', page_title="Arena Results", page_greeting=page_greeting, myHero=myHero, arena=arena)  # return a string
 
 @app.route('/level_up')
 @login_required
 def leveling_up():
     page_greeting = "You have leveled up! How would you like to spend your attribute points?"
-    return render_template('profile.html', page_title="Level Up", page_greeting=page_greeting, myHero=myHero, leveling_up=leveling_up, sidebar=sidebar)  # return a string
+    return render_template('home.html', page_title="Level Up", page_greeting=page_greeting, myHero=myHero, leveling_up=leveling_up)  # return a string
 
 
 @app.route('/battle')
@@ -98,39 +85,42 @@ def battle():
     game.set_enemy(enemy)
     myHero.hp,game.enemy.hp = battle_logic(myHero,game.enemy)
     if myHero.hp == 0:
-        return redirect(url_for('defeat', myHero=myHero))
+        page_title = "Defeat!"
+        page_greeting = "You have died."
     elif game.enemy.hp == 0:
         myHero.current_exp += game.enemy.level * 5
         myHero.level_up(myHero.attribute_points, myHero.current_exp, myHero.max_exp)
-        return redirect(url_for('victory', myHero=myHero))
-    return render_template('battle.html', page_title="Battle", myHero=myHero, game=game, sidebar=sidebar)  # return a string
+        page_title = "Victory!"
+        page_greeting = "You have won the battle!"
+    return render_template('home.html', page_title=page_title, page_greeting=page_greeting, myHero=myHero)  # return a string
+
 
 @app.route('/defeat')
 @login_required
 def defeat():
-    return render_template('profile.html', page_title="Death", myHero=myHero, sidebar=sidebar)  # return a string
+    return render_template('home.html', page_title="Death", myHero=myHero)  # return a string
 
 @app.route('/victory')
 @login_required
 def victory():
-    return render_template('profile.html', page_title="Victory!", myHero=myHero, sidebar=sidebar)  # return a string
+    return render_template('home.html', page_title="Victory!", myHero=myHero)  # return a string
 
 @app.route('/store_greeting')
 @login_required
 def store_greeting():
-    return render_template('store.html', myHero=myHero, is_store_greeting=True, sidebar=sidebar)  # return a string
+    return render_template('store.html', myHero=myHero, is_store_greeting=True)  # return a string
 
 @app.route('/store_armoury')
 @login_required
 def store_armoury():
     items_for_sale = ["ripped tunic", "torn tunic"]
-    return render_template('store.html', myHero=myHero, is_store_armoury=True, items_for_sale=items_for_sale, sidebar=sidebar)  # return a string
+    return render_template('store.html', myHero=myHero, is_store_armoury=True, items_for_sale=items_for_sale)  # return a string
 
 @app.route('/store_weaponry')
 @login_required
 def store_weaponry():
     items_for_sale = ["sword", "axe"]
-    return render_template('store.html', myHero=myHero, is_store_weaponry=True, items_for_sale=items_for_sale, sidebar=sidebar)  # return a string
+    return render_template('store.html', myHero=myHero, is_store_weaponry=True, items_for_sale=items_for_sale)  # return a string
 
 @app.route('/createcharacter', methods=['GET', 'POST'])
 @login_required
@@ -139,13 +129,6 @@ def createcharacter():
         myHero.choose_class()
         return redirect(url_for('profile'))
     return render_template('createcharacter.html', myHero=myHero, sidebar=sidebar)  # return a string
-
-sidebar = {"Arena": "/arena",
-         "Profile": "/profile",
-         "Store": "/store_greeting",
-         "Logout": "/logout",
-         "Battle": "/arena"}
-sidebar = OrderedDict(sidebar)
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
