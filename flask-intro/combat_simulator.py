0a5e9fe5ceba51e82fc7a59cc87f096f7b6f0033
Elthran <jacobbrunner@gmail.com>
2017-01-11 21:38:50 +0800

    Combat simulator framework is finished.

--- flask-intro/combat_simulator.py
+++ flask-intro/combat_simulator.py
@@ -24,23 +24,23 @@ def determine_attacker_defender(character1 ,character2):
 
 def determine_attack_success(attacker, defender):
     """ Determines if the hit is successful or not """
-    print("\n" + attacker.name + " is attempting to attack " + defender.name)
-    if attacker.attack_accuracy < random.randint(0,10):
-        print(attacker.name + " misses " + defender.name)
-        return False
-    if defender.evade_chance >= random.randint(0,20):
-        print(defender.name + " has dodged the attack!")
-        return False
-    if defender.parry_chance >= random.randint(0,20):
-        print(defender.name + " has parried the attack!")
-        return False
-    print("                           " + attacker.name + " has HIT!!!!!!")
+    if attacker.attack_accuracy < random.randint(0,25):
+        return (attacker.name + " misses " + defender.name)
+    if defender.evade_chance >= random.randint(0,25):
+        return (defender.name + " has dodged an attack!")
+    if defender.parry_chance >= random.randint(0,25):
+        return (defender.name + " has parried an attack!")
     return True
 
 def determine_block_success(defender):
     """ Determines if the hit is blocked or not """
-    if defender.block_chance >= random.randint(0,20):
-        print(defender.name + " has blocked the attack!")
+    if defender.block_chance >= random.randint(0,50):
+        return True
+    return False
+
+def determine_critical_hit_success(attacker):
+    """ Determines if the hit is a critical hit or not. Can only happen if it's not blocked by the defender. """
+    if attacker.critical_hit_chance >= random.randint(0,10):
         return True
     return False
 
@@ -48,33 +48,48 @@ def determine_raw_damage(attacker):
     """ Determines how much damage the attacker will hit for """
     raw_attack_damage = random.randint(attacker.minimum_damage, attacker.maximum_damage)
     raw_attack_damage = round(raw_attack_damage, 3)
-    print(attacker.name + " hits for " + str(raw_attack_damage) + " damage.")
     return raw_attack_damage
 
+def determine_raw_critical_hit_damage(attacker):
+    """ Determines the modifier for adjusting how much damage the attacker hits for """
+    critical_hit_modifier = attacker.critical_hit_modifier + (random.randint(0,10) / 20)
+    critical_hit_modifier = round(critical_hit_modifier, 2)
+    return critical_hit_modifier
+
 def determine_modified_damage(defender, raw_damage):
-    """ Determines how much damage the defender will actually take after defence bonuses """
+    """ Determines how much damage the defender will actually take after defensive bonuses """
     modified_attack_damage = raw_damage * (1 - defender.defence_modifier / 100)
     modified_attack_damage = round(modified_attack_damage, 3)
-    print(defender.name + " loses " + str(modified_attack_damage) + " health.")
     return modified_attack_damage
 
 def battle_logic(character1 ,character2):
     """ Runs the entire battle simulator """
-    print(character1.name + " Health: " + str(character1.current_health) + "\n" + character2.name + " Health: " + str(character2.current_health) + "\n")
+    combat_log = [(character1.name + " Health: " + str(character1.current_health) + "\n" + character2.name + " Health: " + str(character2.current_health) + "\n")]
     while character1.current_health > 0 and character2.current_health > 0:
         attacker,defender = determine_attacker_defender(character1, character2)
-        if determine_attack_success(attacker, defender):
+        attack_success = determine_attack_success(attacker, defender)
+        if attack_success == True:
             raw_attack_damage = determine_raw_damage(attacker)
             if determine_block_success(defender):
                 raw_attack_damage = raw_attack_damage * (100 - defender.block_reduction) / 100
+                combat_log.append(defender.name + " has blocked the attack!")
+            elif determine_critical_hit_success(attacker):
+                critical_hit_modifier = determine_raw_critical_hit_damage(attacker)
+                raw_attack_damage *= critical_hit_modifier
+                combat_log.append(attacker.name + " has critically hit, getting a " + str(critical_hit_modifier) + " damage modifier!!")
             modified_attack_damage = determine_modified_damage(defender, raw_attack_damage)
             modified_attack_damage = int(round(modified_attack_damage))
-            defender.current_health -= modified_attack_damage   
-            print(attacker.name + " Health: " + str(attacker.current_health) + "\n" + defender.name + " Health: " + str(defender.current_health))
+            defender.current_health -= modified_attack_damage
+            combat_log.append(attacker.name + " hits for " + str(modified_attack_damage) + " damage. " + defender.name + " now has " + str(defender.current_health) + " health.")   
+        else:
+            combat_log.append(attack_success)
     if character1.current_health <= 0:
-        print(character1.name + " is dead")
+        combat_log.append(character1.name + " is dead")
     else:
-        print(character2.name + " is dead")
+        combat_log.append(character2.name + " is dead")
+        
+    for item in combat_log:
+        print(item)
 
 
 
