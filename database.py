e0ba3fa45064023c5706fafdf28b607b27bcd806
Marlen Brunner <klondikemarlen@gmail.com>
2017-06-16 19:17:10 -0700

    Fix and update database tests suite.

--- database.py
+++ database.py
@@ -3,26 +3,29 @@ I have as of January 1st, 2017 come across a problem where I could not
 store python objects conveniently in my version of the database.
 
 To solve this I am rewriting the whole thing with SQLAlchemy ORM.
-Mainly using the tutorial at: http://docs.sqlalchemy.org/en/latest/orm/tutorial.html
+Mainly using the tutorial at:
+    http://docs.sqlalchemy.org/en/latest/orm/tutorial.html
 
 """
 import hashlib
-import datetime
-import imp
+import importlib
 import os
 import datetime
-import pdb #Testing only
+# Testing only
+import pdb
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sqlalchemy
+import sqlalchemy.exc
 
-#Base is the initialize SQLAlchemy base class. It is used to set up the table metadata.
-#Used like so in the __init__ method: Base.metadata.create_all(engine)
-#What this actually means or does I have no idea but it is neccessary. And I know how to use it.
-#!Important!: Base can only be defined in ONE location and ONE location ONLY!
+# Base is the initialize SQLAlchemy base class. It is used to set up the
+# table metadata.
+# Used like so in the __init__ method: Base.metadata.create_all(engine)
+# What this actually means or does I have no idea but it is neccessary.
+# And I know how to use it.
+# !Important!: Base can only be defined in ONE location and ONE location ONLY!
 import base_classes
-#Internal game modules
+# Internal game modules
 from game import User, Hero, Inbox
 from abilities import Ability
 from locations import Location, WorldMap, Town, Cave
@@ -33,7 +36,7 @@ import complex_relationships
 import prebuilt_objects
 
 
-#Constants#
+# Constants#
 SECOND_PER_ENDURANCE = 10
 
 
@@ -45,12 +48,14 @@ class EZDB:
     
     All add_* methods should end with a commit!
     """
-    def __init__(self, database='sqlite:///:memory:', debug=True, testing=False):
-        """Create a basic sqlalchemy engine and session.
+    def __init__(self, database="sqlite:///:memory:", debug=True,
+                 testing=False):
+        """Create a basic SQLAlchemy engine and session.
         
         Attribute "file_name" is used to find location of database for python.
         
-        Hidden method: _delete_database is for testing and does what it sounds like it does :).
+        Hidden method: _delete_database is for testing and does what it
+        sounds like it does :).
         """
         engine = create_engine(database, echo=debug)
         self.file_name = database[10:]
@@ -68,16 +73,20 @@ class EZDB:
         
         If one is already there then igore and continue.
         Note: each prebuilt_object must be a list.
-        NOTE2: users must come first as it somehow gets built before it gets built if it doesn't?
-        Maybe becuase .. it has a hero which has a current_world? So when current_world gets
-        built then the user gets built too? Which may mean most of my code here is redundant
+        NOTE2: users must come first as it somehow gets built before it gets
+        built if it doesn't?
+        Maybe becuase .. it has a hero which has a current_world? So when
+        current_world gets
+        built then the user gets built too? Which may mean most of my code
+        here is redundant
         and I only really need to build the users list?
         """
         
-        global prebuilt_objects
-        imp.reload(prebuilt_objects)
+        # global prebuilt_objects
+        importlib.reload(prebuilt_objects)
         
-        for obj_list in [prebuilt_objects.users,
+        for obj_list in [
+                prebuilt_objects.users,
                 prebuilt_objects.game_worlds,
                 prebuilt_objects.all_abilities,
                 prebuilt_objects.all_store_items,
@@ -86,21 +95,21 @@ class EZDB:
             for obj in obj_list:
                 try:
                     if isinstance(obj, User):
-                        obj.password = hashlib.md5(obj.password.encode()).hexdigest()
+                        obj.password = hashlib.md5(
+                            obj.password.encode()).hexdigest()
                         obj.timestamp = EZDB.now()
                     self.session.add(obj)
                     self.session.commit()
                 except sqlalchemy.exc.IntegrityError:
                     self.session.rollback()
                     
-    
-    def delete_item(self, id):
+    def delete_item(self, item_id):
         """Delete a given object from the database.
         """
-        self.session.query(Item).filter(Item.id == id).delete()
+        self.session.query(Item).filter(Item.id == item_id).delete()
         self.session.commit()
         
-    def get_object_by_id(self, obj_name, id):
+    def get_object_by_id(self, obj_name, obj_id):
         """Return an object given its class name and id.
         
         Return error if name doesn't exist in global scope. 
@@ -110,26 +119,27 @@ class EZDB:
         """
         try:
             obj = globals()[obj_name.capitalize()]
-            #test if obj is a class.?
-            return self.session.query(obj).get(id)
+            # test if obj is a class.?
+            return self.session.query(obj).get(obj_id)
         except IndexError:
-            raise Exception("Object name: '{}' is not an".format(obj_name) + \
-                "object, or has not been imported into 'database' module yet.")
+            raise Exception(
+                "Object name: '{}' is not an "
+                "object, or has not been imported into "
+                "'database' module yet.".format(obj_name))
         
-    def get_proficiency_by_id(self, id):
+    def get_proficiency_by_id(self, prof_id):
         """Return a proficiency object by id."""
-        return self.session.query(Proficiency).get(id)
+        return self.session.query(Proficiency).get(prof_id)
     
-    def get_item_by_id(self, id):
+    def get_item_by_id(self, item_id):
         """Return an item from its ID.
         """
-        return self.session.query(Item).get(id)
+        return self.session.query(Item).get(item_id)
                     
-                
-    def create_item(self, id):
+    def create_item(self, item_id):
         """Create a new item from a given template name.
         """
-        template = self.session.query(ItemTemplate).get(id)
+        template = self.session.query(ItemTemplate).get(item_id)
         item = Item(template)
         return item
         
@@ -144,24 +154,25 @@ class EZDB:
 
         return self.session.query(Ability).order_by(Ability.name).all()
         
-        
     def get_all_store_items(self):
         """Return all items in the database ordered by name.
         """
-        return self.session.query(ItemTemplate).filter(ItemTemplate.type != "Consumable").order_by(ItemTemplate.name).all()
-        
+        return self.session.query(
+            ItemTemplate).filter(
+            ItemTemplate.type != "Consumable").order_by(
+            ItemTemplate.name).all()
         
     def get_all_marketplace_items(self):
         """Not Implemented!
         """
-        return self.session.query(ItemTemplate).filter_by(type="Consumable").all()
+        return self.session.query(
+            ItemTemplate).filter_by(type="Consumable").all()
         
-
     def get_default_world(self):
         """Get the default world for starting heroes.
         """
-        return self.session.query(WorldMap).filter_by(name="Test_World2").first()
-    
+        return self.session.query(
+            WorldMap).filter_by(name="Test_World2").first()
     
     def get_default_location(self):
         """Get the default location for starting heroes.
@@ -171,13 +182,13 @@ class EZDB:
     def get_default_quests(self):
         """Get the default quests for starting heroes.
         
-        Currently gets quests id's 1 and 3, these are start of two testing quests
+        Currently gets quests id's 1 and 3,
+        these are the start's of two testing quests
         located prebuilt_object.py
         """
 
-        return self.session.query(Quest).filter(Quest.id.in_((1,3))).all()
-        
-        
+        return self.session.query(Quest).filter(Quest.id.in_((1, 3))).all()
+
     def get_user_id(self, username):
         """Return the id of the user by username from the User's table.
         
@@ -186,7 +197,6 @@ class EZDB:
         if user is None:
             return None
         return user.id
-
     
     def get_user_by_username(self, username):
         return self.session.query(User).filter_by(username=username).first()
@@ -199,7 +209,7 @@ class EZDB:
         
         hashed_password = hashlib.md5(password.encode()).hexdigest()
         user = User(username=username, password=hashed_password, email=email,
-            timestamp=EZDB.now())
+                    timestamp=EZDB.now())
         self.session.add(user)
         self.session.commit()
         return user
@@ -221,58 +231,70 @@ class EZDB:
             return user.password == hashlib.md5(password.encode()).hexdigest()
         return None
         
-        
     def fetch_hero_by_username(self, username, character_name=None):
-        """Return live hero objected based on username_or_id and character_name.
+        """Return hero objected based on username_or_id and character_name.
         
         If no character_name is passed just return first hero.
-        Note: Providing a username when you have the hero/character id is redundant.
+        Note: Providing a username when you have the hero/character id is
+        redundant.
         """
         user_id = self.get_user_id(username)
         if character_name is not None:
-            return self.session.query(Hero).filter_by(user_id=user_id, character_name=character_name).first()
+            return self.session.query(
+                Hero).filter_by(
+                user_id=user_id, character_name=character_name).first()
         return self.session.query(User).filter_by(id=user_id).first().heroes[0]
         
-    def fetch_hero_by_id(self, id):
-        return self.session.query(Hero).get(id)
+    def fetch_hero_by_id(self, hero_id):
+        return self.session.query(Hero).get(hero_id)
     
     def update(self):
         """Commit current session.
         
-        NOTE: update function is now mostly redundant! Only use on program exit or logout.
-        When you edit the hero ... he stayes edited! Using any of the other methods will push him to database.
+        NOTE: update function is now mostly redundant!
+        Only use on program exit or logout.
+        When you edit the hero ... he stays edited!
+        Using any of the other methods will push him to database.
         """
         self.session.commit()
-        
+
+    @staticmethod
     def now():
         """Return current UTC time as datetime object in string form.
-        
-        NOTE: I am using UTC as we are working in different time zones and I think it might screw up otherwise.
+
+        NOTE: I am using UTC as we are working in different time
+        zones and I think it might screw up otherwise.
         """
         return datetime.datetime.utcnow()
-    
-    #Marked for renaming as it effects Hero endurance as well as time.
-    #Consider update_endurance_and_time()
-    #Or update_game_clock
-    #Or update_hero_clock
+
+    # Marked for renaming as it effects Hero endurance as well as time.
+    # Consider update_endurance_and_time()
+    # Or update_game_clock
+    # Or update_hero_clock
     def update_time(self, hero):
         """Update the game time clock of a specific Hero and endurance values.
         
-        This increases the hero's endurance by the difference between past timestamp and current time.
-        NOTE: Updates current timestamp in charater table but only if has been incremented.
+        This increases the hero's endurance by the difference between past
+        timestamp and current time.
+        NOTE: Updates current timestamp in character table but only if has
+        been incremented.
         Which may not be a good idea but ...
         
-        Suggestion: Currently only affects the passed Hero, perhaps it should update all heros?
+        Suggestion: Currently only affects the passed Hero, perhaps it
+        should update all heroes?
         """
         timestamp = hero.timestamp
         time_diff = (EZDB.now() - timestamp).total_seconds()
         endurance_increment = int(time_diff / SECOND_PER_ENDURANCE)
         hero.proficiencies.endurance.current += endurance_increment
             
-        if hero.proficiencies.endurance.current > hero.proficiencies.endurance.maximum:
-            hero.proficiencies.endurance.current = hero.proficiencies.endurance.maximum
-            
-        if endurance_increment: #Only update if endurance has been incremented.
+        if hero.proficiencies.endurance.current \
+                > hero.proficiencies.endurance.maximum:
+            hero.proficiencies.endurance.current \
+                = hero.proficiencies.endurance.maximum
+
+        # Only update if endurance has been incremented.
+        if endurance_increment:
             hero.timestamp = EZDB.now()
         self.session.commit()
         
@@ -289,15 +311,7 @@ class EZDB:
         try:
             os.remove(self.file_name)
         except FileNotFoundError:
-            #Ignore because the file has already been deleted.
+            # Ignore because the file has already been deleted.
             pass
         except PermissionError:
             pass
-                            
-                        
-if __name__ == "__main__":
-    import tests.database_tests.py
-    
-    
-    
-    
