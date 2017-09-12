0d581ec10f4b28dbff1b8e874157348c2828ad37
Marlen Brunner <klondikemarlen@gmail.com>
2017-09-12 10:24:15 -0700

    Add in new 'specifications folder'.
    This will be like a README for what much of the code in the game is _supposed_ to do. It will probably be chronically out of date or just plain wishful thinking.
    On the other hand, I hope that it will provide me with some structure and a way to trace out and decribe 'code paths' (i.e. the path that data flows to execute some kind of sensible result)
    Yay! I have a working event/trigger analysis.

---
+++ specifications/event_trigger_analysis.py
@@ -0,0 +1,115 @@
+# Move event occurs - CREATION PHASE, PHASE I
+# app.py
+@app.route('/store/<name>')
+@login_required
+@uses_hero_and_update
+@update_current_location
+def store(name, hero=None, location=None):
+    engine.spawn('move_event', hero, description="The Hero visits a store.")
+
+
+# Move event is built as an object and stored for later (maybe journal)
+# BUILD PHASE, PHASE II
+# engine.py
+def spawn(self, event_name, hero, *args, description=None):
+    event = Event(event_name, hero_id=hero.id, description=description)
+    self.db.add_object(event)
+
+
+    # Test to see if any triggers are in place to handle this event
+    # Note that the trigger must be "pre-built".
+    # Set all triggers with valid Conditions to 'completed'
+    # TRIGGER PHASE, PHASE III
+    triggers = self.db.get_all_triggers_by(event_name, hero.id)
+    for trigger in triggers:
+        trigger.evaluate()
+
+
+    # This should find all database objects that have completed triggers
+    # and run any code they have that is set to run if a trigger completes.
+    # This needs some work.
+    # HANDLE PHASE, PHASE IV
+    # TODO ... make this for all objects with completed_triggers .. ?
+    objects = self.db.get_all_quest_paths_with_completed_triggers(hero)
+    # return the "Blacksmith" quest object ...
+    # Since its completion trigger is completed ...
+    # It is now completed. Run the method that you run when trigger
+    # completes.
+    for obj in objects:
+        obj.run_if_trigger_completed()
+
+    # When all code has been run, save everything.
+    # UPDATE PHASE, PHASE V
+    self.db.update()
+
+
+"""
+#################
+uses Triggers decorator?
+NOTE: currently only a theory.
+I might need to combine decorators and meta-classing?
+Here I am trying to decorate a class with a class.
+The basic steps are:
+    1. add completion trigger to __init__
+    2. add link/try except.
+    3. add relationship to trigger (back_population my be unneeded)
+    4. add run_if_completed code.
+    4a. unlink old trigger
+    4b. do the thing that the whole party if for (e.g. quest.advance())
+    4c. link next trigger if available.
+################
+"""
+class Decorator(object):
+    def __init__(self, arg):
+        self.arg = arg
+        # Add connection to cls init
+        completion_trigger = None
+
+        # Make Trigger available!
+        # Some kind of linkage between trigger and hero.
+        # Instantiated triggers must relate to a specific hero.
+        try:
+            self.quest.completion_trigger.link(hero)
+        except AttributeError as ex:
+            print("Warning: The quest with the description '{}' has no "
+                  "completion trigger set up.".format(quest.description))
+            print('See error "{}"'.format(ex))
+
+    def __call__(self, cls):
+        class Wrapped(cls):
+            classattr = self.arg
+
+            # Add relationship to cls spec.
+            trigger_id = Column(Integer, ForeignKey('trigger.id'))
+            completion_trigger = relationship("Trigger",
+                                              back_populates='quests')
+
+            # Add generic run if completed method to new class.
+            def run_if_trigger_completed(self):
+                """Deactivate trigger and run activation code.
+
+                In this case ... unlink the trigger from the given quest
+                and the advance the quest to the next stage.
+
+                In theory ... this would bring the new trigger in to play
+                through activation of a new quest? Or have I not set that up right?
+                """
+                self.quest.completion_trigger.unlink()
+                self.advance()
+                # Make Trigger available!
+                try:
+                    self.quest.completion_trigger.link(self.hero)
+                except AttributeError as ex:
+                    print(
+                        "Warning: The quest with the description '{}' has no "
+                        "completion trigger set up.".format(
+                            self.quest.description))
+                    print('See error "{}"'.format(ex))
+
+        return Wrapped
+
+
+@Decorator("decorated class")
+class SomeClassThatUsesTriggers:
+    pass
+
