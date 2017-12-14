355750c9d1aed633e99261398f9b44f967d3ebbc
Marlen Brunner <klondikemarlen@gmail.com>
2017-12-13 16:47:19 -0800

    Improve Choose Character page.
    It still is bugged .. Bad Request.

--- app.py
+++ app.py
@@ -118,13 +118,23 @@ def prevent_url_typing(f):
 
 
 @app.template_filter()
-def handle_undefined_error(obj):
+def validate_hero_image(hero):
     """Used to handle an expected/possible error in the template.
 
     See https://codenhagen.wordpress.com/2015/08/20/custom-jinja2-template-filters-and-flask/
     """
-    pdb.set_trace()
-    return None
+    # pdb.set_trace()
+    image_name = ''
+    try:
+        image_name = "archetype_{}.jpg".format(hero.archetype.lower())
+    except AttributeError:
+        image_name = "character.jpg"
+    return image_name
+
+
+@app.template_filter()
+def validate_hero_name(hero):
+    return hero.name if hero.name else "UnNamed"
 
 
 @app.route('/favicon.ico')
