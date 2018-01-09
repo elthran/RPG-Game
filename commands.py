db2d93de47019f6c64778a650004de243a473175
Marlen Brunner <klondikemarlen@gmail.com>
2018-01-09 10:54:33 -0800

    Update the commands.py documentation.
    Fix inbox usage of sendToPy().

--- commands.py
+++ commands.py
@@ -7,11 +7,86 @@ class Command:
     """Run a list of html update commands based on the string cmd.
     
     Usage:
-    in app.py
-    
+    # 'app.py'
     from commands import Command
-    
-    @app.route('/<cmd>')
+    NEW STYLE
+    @app.route('/command/<cmd>')
+    command_function = Command.cmd_functions(cmd)
+        if request.method == 'POST' and request.is_json:
+            data = request.get_json()
+            response = command_function(hero, database, data=data,
+                                            engine=engine)
+        return response
+
+    # 'commands.py'
+    @staticmethod
+    def buy(hero, database, data, engine):
+        item_id = data['id']
+        location = data['location']
+        item = database.create_item(item_id)
+        if hero.gold >= item.buy_price:
+            hero.inventory.add_item(item)
+            hero.gold -= item.buy_price
+            engine.spawn(
+                'buy_event',
+                hero,
+                description="{} buys a/an {}.".format(hero.name, item.name)
+            )
+            return jsonify(
+                message="Purchased: {}: id={}".format(item.name, item.id),
+                heroGold=hero.gold)
+        return jsonify(error="Not enough gold to buy '{}'!".format(item.name))
+
+    # 'store.html'
+    <button onclick="sendToPy(
+        event,
+        itemPurchasedPopup, 'buy', {'id': {{ item.id }}});">Buy</button>
+    NOTE: the data is sent as JSON
+
+    # 'script.js'
+    See:
+    function sendToPy(event, callback, cmd, data, preProcess, url) {
+    "use strict";
+    var element = event.target;
+    ...
+    if (cmd) {
+        url = "/command/" + cmd;
+    } else if (!cmd && !url) {
+        // if url is blank use url of page
+        url = window.location.pathname;
+    }
+
+    // Normal data processing is object form.
+    // Must return a JSON object - no I can't check for this.
+    if (preProcess) {
+        data = preProcess(element);
+    }
+
+    // If you send a simple string location won't be added.
+    // If you send (hopefully) some JSON the location parameter will be added.
+    if (typeof data !== "string") {
+        // If there is some (JSON type data) add in the location variable.
+        data.location = window.location.pathname;
+    }
+
+    postJSON(url, data, callback);
+    ...
+    }
+
+    #NOTES:
+        Go to the real code for most up to date documentation and examples.
+
+    -preProcess is the name of whatever JS function you want to use
+    to extract the data from the HTML. See 'scripts.js > getIdsFromCheckboxes'
+    See: 'inbox.html'
+    <input type="submit" name="delete" form="messageForm" value="DELETE"
+    onclick="return sendToPy(
+        event, updateMessageTable, null, null, getIdsFromCheckboxes);"/>
+
+
+
+    OLD STYLE
+    @app.route('/command/<cmd>')
     def command(cmd=None):
         try:
             # command_function = getattr(Command, <cmd>)
