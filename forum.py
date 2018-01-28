9149b9c1eb9fc17eec77fd6d367a613432591740
Marlen Brunner <klondikemarlen@gmail.com>
2018-01-28 13:21:58 -0800

    Basic forum relationships.
    Bugged. Need to fix Object creation and display in HTML and Prebuild objects.

--- forum.py
+++ forum.py
@@ -1,3 +1,5 @@
+import datetime
+
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
@@ -9,37 +11,56 @@ class Forum(Base):
     __tablename__ = 'forum'
 
     id = Column(Integer, primary_key=True)
-    threads = []
+
+    # Relationships
+    # Many to One with Thread
+    threads = relationship("Thread", back_populates="forum")
 
     def create_thread(self, thread):
         self.threads.append(thread)
+
 
 class Thread(Base):
     __tablename__ = "thread"
 
     id = Column(Integer, primary_key=True)
+
+    # Relationships
+    # One to many with Forum
+    forum_id = Column(Integer, ForeignKey('forum.id'))
+    forum = relationship("Forum", back_populates="threads")
+
+    # Many to One with Posts
+    posts = relationship("Post", back_populates="thread")
+
     title = Column(String)
-    posts = []
     creator = Column(String)
     timestamp = Column(DateTime)
 
-    def __init__(self, title="unnamed thread", creator="None", timestamp="Unknown Time"):
+    def __init__(self, title="unnamed thread", creator="None"):
         self.title = title
         self.creator = creator
-        self.timestamp = timestamp
+        self.timestamp = datetime.datetime.utcnow()
 
     def write_post(self, post):
         self.posts.append(post)
+
 
 class Post(Base):
     __tablename__ = "post"
 
     id = Column(Integer, primary_key=True)
+
+    # Relationships
+    # One to Many with Thread.
+    thread_id = Column(Integer, ForeignKey('thread.id'))
+    thread = relationship("Thread", back_populates="posts")
+
     content = Column(String)
     author = Column(String)
     timestamp = Column(DateTime)
 
-    def __init__(self, content="Error: Content missing", author="Unknown author", timestamp="Unknown Time"):
+    def __init__(self, content="Error: Content missing", author="Unknown author"):
         self.content = content
         self.author = author
-        self.timestamp = timestamp
+        self.timestamp = datetime.datetime.utcnow()
