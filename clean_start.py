a1a61efd5a7fe2c5070e3ef87e6850757fd84890
Marlen T. B <klondikemarlen@gmail.com>
2018-01-31 20:09:26 -0800

    Add in comments lost in merge.
    Reformat lost comments. Update debug statement.

--- clean_start.py
+++ clean_start.py
@@ -1,6 +1,9 @@
 import os
 # os.system("del static\database.db")
 os.system("cls")
+# Custom drop/rebuild database table here.
+# See alembic!
+print("Take a look at Alembic (and 'clean_start.py' code) if you get database errors!")
 try:
     os.system("app.py")
 except KeyboardInterrupt:
