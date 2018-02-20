309945a5abc45606ef41df7a1beade7fd5be3891
Marlen Brunner <klondikemarlen@gmail.com>
2018-02-19 18:19:03 -0800

    Renable html build_code.py.
    I really want to use "template discovery" ... like pytest test discovery but for build_codage :P

--- build_code.cmd
+++ build_code.cmd
@@ -3,7 +3,7 @@ PUSHD abstraction_lv1
 python build_code.py
 POPD
 REM Second build code engine .. build_code.py(s) are not the same.
-REM PUSHD templates\abstraction_lv1
-REM python build_code.py
-REM POPD
+PUSHD templates\abstraction_lv1
+python build_code.py
+POPD
 echo Code built!
