534309fa3879a06b44c5860f393f3849d5897a59
Marlen Brunner <klondikemarlen@gmail.com>
2018-02-20 18:27:16 -0800

    Update 'build_code.py' to use automatic 'abstraction_lv' detection!
    Now I only have one version of build_code.py!
    Add basic test in test_build_code.py (by no means a complete test suite).
    Update various code to use 'os.sep' for better portability.
    
    NOTE: the profile_proficiencies.html template is _way_ out of date.
    
    I am considering switching to a different templating language as Jinja2 doesn't allow very sensible indent control.

--- alembic/unused_versions/7c0ce008d107_fix_cascading_deletes.py
+++ alembic/unused_versions/7c0ce008d107_fix_cascading_deletes.py
@@ -16,8 +16,8 @@ from alembic import op
 import sqlalchemy as sa
 
 # Get the name of the current directory for this file and split it.
-old_path = os.path.dirname(os.path.abspath(__file__)).split('\\')
-new_path = '\\'.join(old_path[:-1])
+old_path = os.path.dirname(os.path.abspath(__file__)).split(os.sep)
+new_path = os.sep.join(old_path[:-1])
 # -1 refers to how many levels of directory to go up
 # In this case add a new path at 'alembic' directory.
 sys.path.insert(0, new_path)
