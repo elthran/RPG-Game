dd7be7187dc223e68f0e0c551f7d5987e3a37742
Marlen Brunner <klondikemarlen@gmail.com>
2018-02-22 08:34:26 -0800

    Add in initial (broken?) code from Corentin Wallez.
    From their blog https://blog.kangz.net/posts/2016/08/31/code-generation-the-easier-way/.
    I indend to fix it up and rebuild it in such a way that it will work for me. Maybe I'll even make it a python module installable by 'pip'. Since It seems as though I am not the only person with this problem.

--- build_code.py
+++ build_code.py
@@ -1,11 +1,14 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-
 import importlib
 import os
 import stat
 import hashlib
 from contextlib import contextmanager
 import sys
+import re
+import pdb
+
+import jinja2
+from jinja2 import Environment, select_autoescape
 
 LANGUAGES = {
     '.html': {
@@ -21,6 +24,57 @@ LANGUAGES = {
         },
     },
 }
+
+
+# A custom Jinja2 template loader that removes the extra indentation
+# of the template blocks so that the output is correctly indented
+# Compliments of https://blog.kangz.net/posts/2016/08/31/code-generation-the-easier-way/
+class PrependIndentLoader(jinja2.BaseLoader):
+    def __init__(self, path):
+        self.path = path
+
+    def get_source(self, environment, template):
+        path = os.path.join(self.path, template)
+        if not os.path.exists(path):
+            raise jinja2.TemplateNotFound(template)
+        mtime = os.path.getmtime(path)
+        with open(path) as f:
+            source = self.preprocess(f.read())
+        return source, path, lambda: mtime == os.path.getmtime(path)
+
+    blockstart = re.compile('{%-?\s*(if|for|block)[^}]*%}')
+    blockend = re.compile('{%-?\s*end(if|for|block)[^}]*%}')
+
+    def preprocess(self, source):
+        pdb.set_trace()
+        lines = source.split('\n')
+
+        # Compute the current indentation level of the template blocks and remove their indentation
+        result = []
+        indentation_level = 0
+
+        for line in lines:
+            # The capture in the regex adds one element per block start or end so we divide by two
+            # there is also an extra line chunk corresponding to the line end, so we substract it.
+            numends = (len(self.blockend.split(line)) - 1) // 2
+            indentation_level -= numends
+
+            result.append(self.remove_indentation(line, indentation_level))
+
+            numstarts = (len(self.blockstart.split(line)) - 1) // 2
+            indentation_level += numstarts
+
+        return '\n'.join(result)
+
+    def remove_indentation(self, line, n):
+        for _ in range(n):
+            if line.startswith(' '):
+                line = line[4:]
+            elif line.startswith('\t'):
+                line = line[1:]
+            else:
+                assert(line.strip() == '')
+        return line
 
 
 def fix_camel_case(name):
@@ -135,7 +189,7 @@ def build_templates(filenames, extension):
         extension = "." + extension
 
     env = Environment(
-        loader=FileSystemLoader(''),
+        loader=PrependIndentLoader(''),
         autoescape=select_autoescape(default_for_string=False, default=False),
         trim_blocks=True,
         lstrip_blocks=True,
