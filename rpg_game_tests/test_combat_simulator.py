7d4dac97485ed26325d0cd0f1606e3a835fb832f
Marlen T. B <klondikemarlen@gmail.com>
2018-03-08 14:05:37 -0800

    Create test_combat_simulator.py
    Add in description of tests from @elthran.
    
    Formatting by me.

---
+++ rpg_game_tests/test_combat_simulator.py
@@ -0,0 +1,23 @@
+"""
+Test combat simulator. 
+
+1. Should be able to: 
+    a. Simply login, 
+    b. go to "users" page. 
+    c. Click on the other user. 
+    d. Click battle. 
+    e. The console should print out the health of both users each round (after attacks are made). 
+    f. Should stop when someone reaches 0 health. 
+    g. Then revisiting user page should show new healths. 
+    h. If enemy is dead you should no longer be able to battle them.
+    
+If anything else happens (like you are both still alive, they are dead but you can fight them, etc.) then let me know. But I think I fixed all bugs. Just get confused between summed_profs and base profs. Please check and make sure I did it correctly.
+
+2. Can you merge the time branch into master branch if it's fixed? I want to work on it WITH my battle code.
+    a. Like have all players heal every 30 seconds.
+    b. Then mix that in with my battle code.
+    c. Like attack them,
+    d. let them heal,
+    e. attack them again.
+    f. See if there are any bugs. I want to get it all ironed out before I progress
+"""
