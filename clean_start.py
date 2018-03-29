0009442d5734d57f337934b3135347c067930e21
Marlen Brunner <klondikemarlen@gmail.com>
2018-03-29 16:03:51 -0700

    Add in data for migration (mydb_data.sql).
    Add -m option to clean_start.py to build a blank database.

--- clean_start.py
+++ clean_start.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     #                     help="Cleanly start 'app.py' .. possibly rebuild the database.",
     #                     action='store_true')
     parser.add_argument("-f", help="Delete the database.", action='store_true')
+    parser.add_argument("-m", help="Make blank database for migrations.", action='store_true')
     args = parser.parse_args()
     # print(args)
 
@@ -15,6 +16,10 @@ if __name__ == "__main__":
     if args.f:
         os.system('mysql -u elthran -p7ArQMuTUSoxXqEfzYfUR -e "DROP DATABASE IF EXISTS rpg_database;"')
         print("Database deleted!")
+    elif args.m:
+        os.system('mysql -u elthran -p7ArQMuTUSoxXqEfzYfUR -e "DROP DATABASE IF EXISTS rpg_database;"')
+        os.system("python3 -c 'import database;database.EZDB(\"mysql+mysqldb://elthran:7ArQMuTUSoxXqEfzYfUR@localhost/rpg_database\", debug=False, testing=True);print(\"Blank database initialized!\")'")
+        exit(0)
 
     try:
         os.system("app.py")
