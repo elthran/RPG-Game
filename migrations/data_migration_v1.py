bc66d620e9594fb19a7c80e88e42d9f0197b4def
Marlen Brunner <klondikemarlen@gmail.com>
2018-04-02 10:33:07 -0700

    Began work on take 5 of data migration.
    New style uses normal game code to add all objects manually.
    I generated the migration script to help work out what data to transfer.

--- migrations/data_migration_v1.py
+++ migrations/data_migration_v1.py
@@ -12,7 +12,7 @@ new_path = os.sep.join(old_path[:-1])
 sys.path.insert(0, new_path)
 from __init__ import *
 
-import database
+import database as db
 from build_code import normalize_class_name
 sys.path.pop(0)
 
@@ -25,100 +25,129 @@ old_meta.reflect()
 
 old_session = Session(bind=old_engine)
 
-db = database.EZDB("mysql+mysqldb://elthran:7ArQMuTUSoxXqEfzYfUR@localhost/rpg_database", debug=False)
+database = db.EZDB("mysql+mysqldb://elthran:7ArQMuTUSoxXqEfzYfUR@localhost/rpg_database", debug=False)
 
-# generic one.
-# pdb.set_trace()
-# Iterate through all the table objects in the old database's schema.
-for name, table in old_meta.tables.items():
-    # Get class name from table name for dummy object creation.
-    cls_name = normalize_class_name(name)
-    # Return each row in each table. (all the data in the database one row at a time).
-    for old_obj in old_session.query(table).all():
-        try:
-            obj = db.get_object_by_id(cls_name, old_obj.id)
-        except IndexError:
-            obj = None
-            # Need to create a new object as there isn't one to overwrite.
-            # These objects should have been all imported into the database module.
-            try:
-                Class = getattr(database, cls_name)
-            except KeyError:
-                Class = None
-                print("'{}' class not found in database module. Import it there.".format(cls_name))
-            # Make a new dummy object that is then going to be replaced with new fields.
-            # Get the signature of the objects constructor. This will allow me
-            # to create a new object using default arguments.
-            if Class:
-                sig = inspect.signature(Class)
-                # Get some appropriately typed values for the constructor signature.
-                # Should return a list of arguments to pass to the dummy class
-                # constructor.
-                args = [getattr(old_obj, key) if key in old_obj else sig.parameters[key].default if sig.parameters[key].default != sig.empty else None for key in sig.parameters.keys() if key != 'kwargs']
-                print(args)
-                try:
-                    obj = Class(*args)
-                except:
-                    pdb.set_trace()
-                db.session.add(obj)
-        except KeyError:
-            obj = None
-            print("'{}' class not found in database module. Import it there.".format(cls_name))
-        except AttributeError:
-            # This is some kind of association object.
-            # I haven't worked out how to clone the variables safely.
-            # If I could query the new objects by row id?
-            obj = None
-            print(old_obj)
-            if name in ("adjacent_location", "quest_path_to_quest"):
-                continue  # ignore the tables listed above.
-            pdb.set_trace()
-        # Update dummy object with migrated data.
-        if obj:
-            for key in old_obj.keys():
-                value = getattr(old_obj, key)
-                if key == 'polymorphic_identity':
-                    pdb.set_trace()
-                try:
-                    setattr(obj, key, value)
-                except AttributeError:
-                    print("'{}' has no attribute '{}'".format(cls_name, key))
-            db.update()
 
-exit("It works!")
+# Most important is to migrate the user!
+# create a new user with data from old user.
+def migrate_users():
+    old_user_table = old_meta['user']
+    for old_user in old_session.query(old_user_table).all():
+        # don't add in the default users [user.username for user in db.prebuilt_objects.users]
+        # I could also just drop the first 2 user objects?
+        if old_user.username not in ['admin', 'marlen']:
+            user = database.add_new_user(old_user.username, old_user.password, email=old_user.email_address)
+            user.timestamp = old_user.timstamp
+            user.prestige = old_user.prestige
 
-user_table = old_meta.tables['user']
-for old_obj in old_session.query(user_table).all():
-    try:
-        obj = db.get_object_by_id("User", old_obj.id)
-    except IndexError:
-        obj = None
-        # get default args ....
-        # pass them to constructor
-        # This should create a new dummy object which should have the appropriate default arguments.
-        try:
-            Class = getattr(database, "User")
-        except KeyError:
-            Class = None
-            print("User object not found")
-        # pdb.set_trace()
-        # Make a new dummy object that is then going to be replaced with new fields.
-        # Get the signature of the objects constructor
-        if Class:
-            sig = inspect.signature(Class)
-            # Get some appropriately typed values for the constructor signature.
-            args = (getattr(old_obj, key) for key in sig.parameters.keys())
-            obj = Class(*args)
-            db.session.add(obj)
-    # pdb.set_trace()
-    # Update dummy object with migrated data.
-    if obj:
-        for key in old_obj.keys():
-            try:
-                setattr(obj, key, getattr(old_obj, key))
-            except KeyError:
-                pass
-        db.update()
+            database.add_new_hero_to_user(user)
+    """
+    op.add_column('inbox', sa.Column('user_id', sa.Integer(), nullable=True))
+    op.create_foreign_key(op.f('fk_inbox_user_id_user'), 'inbox', 'user', ['user_id'], ['id'], ondelete='CASCADE')
+    op.drop_constraint('fk_post_user_id_user', 'post', type_='foreignkey')
+    op.create_foreign_key(op.f('fk_post_thread_id_thread'), 'post', 'thread', ['thread_id'], ['id'], ondelete='CASCADE')
+    op.create_foreign_key(op.f('fk_post_user_id_user'), 'post', 'user', ['user_id'], ['id'], ondelete='CASCADE')
+    op.add_column('user', sa.Column('reset_key', sa.Unicode(length=200), nullable=True))
+    op.drop_constraint('fk_user_inbox_id_inbox', 'user', type_='foreignkey')
+    op.drop_column('user', 'inbox_id')
+    """
+
+
+if __name__ == "__main__":
+    migrate_users()
+    exit("It didn't crash!")
+
+# # generic one.
+# # pdb.set_trace()
+# # Iterate through all the table objects in the old database's schema.
+# for name, table in old_meta.tables.items():
+#     # Get class name from table name for dummy object creation.
+#     cls_name = normalize_class_name(name)
+#     # Return each row in each table. (all the data in the database one row at a time).
+#     for old_obj in old_session.query(table).all():
+#         try:
+#             obj = db.get_object_by_id(cls_name, old_obj.id)
+#         except IndexError:
+#             obj = None
+#             # Need to create a new object as there isn't one to overwrite.
+#             # These objects should have been all imported into the database module.
+#             try:
+#                 Class = getattr(database, cls_name)
+#             except KeyError:
+#                 Class = None
+#                 print("'{}' class not found in database module. Import it there.".format(cls_name))
+#             # Make a new dummy object that is then going to be replaced with new fields.
+#             # Get the signature of the objects constructor. This will allow me
+#             # to create a new object using default arguments.
+#             if Class:
+#                 sig = inspect.signature(Class)
+#                 # Get some appropriately typed values for the constructor signature.
+#                 # Should return a list of arguments to pass to the dummy class
+#                 # constructor.
+#                 args = [getattr(old_obj, key) if key in old_obj else sig.parameters[key].default if sig.parameters[key].default != sig.empty else None for key in sig.parameters.keys() if key != 'kwargs']
+#                 print(args)
+#                 try:
+#                     obj = Class(*args)
+#                 except:
+#                     pdb.set_trace()
+#                 db.session.add(obj)
+#         except KeyError:
+#             obj = None
+#             print("'{}' class not found in database module. Import it there.".format(cls_name))
+#         except AttributeError:
+#             # This is some kind of association object.
+#             # I haven't worked out how to clone the variables safely.
+#             # If I could query the new objects by row id?
+#             obj = None
+#             print(old_obj)
+#             if name in ("adjacent_location", "quest_path_to_quest"):
+#                 continue  # ignore the tables listed above.
+#             pdb.set_trace()
+#         # Update dummy object with migrated data.
+#         if obj:
+#             for key in old_obj.keys():
+#                 value = getattr(old_obj, key)
+#                 if key == 'polymorphic_identity':
+#                     pdb.set_trace()
+#                 try:
+#                     setattr(obj, key, value)
+#                 except AttributeError:
+#                     print("'{}' has no attribute '{}'".format(cls_name, key))
+#             db.update()
+
+
+# user_table = old_meta.tables['user']
+# for old_obj in old_session.query(user_table).all():
+#     try:
+#         obj = db.get_object_by_id("User", old_obj.id)
+#     except IndexError:
+#         obj = None
+#         # get default args ....
+#         # pass them to constructor
+#         # This should create a new dummy object which should have the appropriate default arguments.
+#         try:
+#             Class = getattr(database, "User")
+#         except KeyError:
+#             Class = None
+#             print("User object not found")
+#         # pdb.set_trace()
+#         # Make a new dummy object that is then going to be replaced with new fields.
+#         # Get the signature of the objects constructor
+#         if Class:
+#             sig = inspect.signature(Class)
+#             # Get some appropriately typed values for the constructor signature.
+#             args = (getattr(old_obj, key) for key in sig.parameters.keys())
+#             obj = Class(*args)
+#             db.session.add(obj)
+#     # pdb.set_trace()
+#     # Update dummy object with migrated data.
+#     if obj:
+#         for key in old_obj.keys():
+#             try:
+#                 setattr(obj, key, getattr(old_obj, key))
+#             except KeyError:
+#                 pass
+#         db.update()
 
 # ability_table = old_meta.tables['hero']
 #
