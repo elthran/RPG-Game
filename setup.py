873e3299ac41332b8ce76860c7ed5356fd330390
Marlen Brunner <klondikemarlen@gmail.com>
2018-05-03 15:51:48 -0700

    Add requirements.txt and setup.py requirements parsing.

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
+requirements = []
+with open('requirements.txt') as f:
+    for line in f:
+        line = line.strip()
+        if line and not line.startswith('#'):
+            line = line.split('==')[0]
+            requirements.append(line)
+
 setup(
     name='elthranonline',
     packages=['elthranonline'],
     include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
+    # install_requires=[
+    #     'flask',
+    # ],
+    install_requires=requirements,
 )
 
 
